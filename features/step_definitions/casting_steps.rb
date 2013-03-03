2f45f70e54eec6e887b5b66a4aa73ad516894038
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-03-03 00:30:13 +0000

    Change step text

--- features/step_definitions/casting_steps.rb
+++ features/step_definitions/casting_steps.rb
@@ -6,6 +6,6 @@ end
 Then /^I will get back six lines$/ do
   @result.size.should eq 6
 end
-When /^each will have a score of between 6 and 9$/ do
-  @result.all? {|line_value| line_value >= 6 and line_value <= 9 }.should be
+When /^each will have a score of between 6 and 9 inclusive$/ do
+  @result.all? {|line_value| (6..9).include? line_value }.should be
 end
