8c6fa30ec0688333d58e95aa73028fe3ac387c90
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-03-20 21:52:26 +0000

    Add blank line

--- features/step_definitions/casting_steps.rb
+++ features/step_definitions/casting_steps.rb
@@ -6,6 +6,7 @@ end
 Then /^I will get back six lines$/ do
   @line_values.size.should eq 6
 end
+
 When /^each will have a score of between 6 and 9 inclusive$/ do
   @line_values.all? {|line_value| (6..9).include? line_value }.should be
 end
