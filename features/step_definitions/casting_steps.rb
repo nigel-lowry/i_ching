cabf63d87762c7504cc713a5abbb4779cf0d33ce
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2016-03-11 19:49:12 +0000

    Reword step

--- features/step_definitions/casting_steps.rb
+++ features/step_definitions/casting_steps.rb
@@ -7,6 +7,6 @@ Then /^I will get back six lines$/ do
   expect(@line_values).to have(6).items
 end
 
-When /^each will have a score of between 6 and 9 inclusive$/ do
+When /^each will have a score between 6 and 9 inclusive$/ do
   expect(@line_values.all? {|line_value| (6..9).include? line_value }).to be_true
 end
