c1623f87d7528ac1326bfef17d5266dcdb6ea751
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2017-06-16 20:10:13 +0100

    Change regular expressions

--- features/step_definitions/lookup_steps.rb
+++ features/step_definitions/lookup_steps.rb
@@ -1,7 +1,7 @@
-Given(/^the hexagram from top to bottom is (.*)$/) do |hexagram|
+Given(/^the hexagram from top to bottom is "(u|b){6}"$/) do |hexagram|
   pending
 end
 
-Then(/^its number is (.*)$/) do |number|
+Then(/^its number is (#{CAPTURE_A_NON_NEGATIVE_INTEGER})$/) do |number|
   pending
 end
